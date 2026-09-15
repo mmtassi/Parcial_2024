28e00c65bfdd2393e31c38d2a4c7142d94a13f85
mmtassi <mtassi@fie.undef.edu.ar>
2026-09-15 11:09:32 -0300

    .

--- README.md
+++ README.md
@@ -12,4 +12,6 @@ empleados de planta: horas trabajadas por dia
                      horas mensuales: 200
                      nivel
                      
+
+en los test en el 4to punto es obtener cual es el mejor sueldo del mes, la consigna esta mal escrita
                      
---
+++ solucion/main.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+class Empleado(ABC):
+    def __init__(self, nombre, apellido, dni , relacion_dependencia, sueldo):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.dni = dni
+        self.relacion_dependencia = relacion_dependencia
+        self.sueldo = sueldo
+
+    @abstractmethod
+    def calcular_sueldo(self):
+        pass
+
+class Contratados(Empleado):
+    def __init__(self, nombre, apellido, dni , relacion_dependencia, sueldo, horas_trabajadas):
+        super().__init__(nombre, apellido, dni , relacion_dependencia, sueldo)
+        self.horas_minimas_dia = 8
+        self.horas_trabajadas = horas_trabajadas
+
+    def calcular_sueldo(self):
+        return self.sueldo * self.horas_trabajadas
+
+class EmpleadoPlanta(Empleado):
+    def __init__(self, nombre, apellido, dni , relacion_dependencia, sueldo, antiguedad):
+        super().__init__(nombre, apellido, dni , relacion_dependencia, sueldo)
+        self.antiguedad = antiguedad
+
+    def calcular_sueldo(self):
+        return self.sueldo + (self.antiguedad * 100)  # Sueldo base más un bono por antigüedad
