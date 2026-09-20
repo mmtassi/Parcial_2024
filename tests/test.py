9f6bf5aaab70c3e836fbdeae7893f1794859fa3c
mmtassi <mtassi@fie.undef.edu.ar>
2026-09-20 15:05:23 -0300

    test

---
+++ tests/test.py
@@ -0,0 +1,21 @@
+import pytest
+from solucion.main import *
+
+
+def test_sueldo_contratado():
+    dependencia = Contratado(8, 100)
+
+    pepe = Empleado("pepe", "argento", 33888999, dependencia)
+    pepe.agregar_horas(10)
+    pepe.agregar_horas(8)
+    pepe.agregar_horas(7)
+    pepe.agregar_horas(7)
+    pepe.agregar_horas(8)
+    pepe.agregar_horas(8)
+    pepe.agregar_horas(8)
+    pepe.agregar_horas(8)
+    pepe.agregar_horas(8)
+    pepe.agregar_horas(8)
+    pepe.agregar_horas(8)
+
+    assert pepe.sueldo() == 7200
